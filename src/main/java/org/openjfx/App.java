4784d9d44698bda465a36724ffa71a909456b967
Iiro <iiro.paakkonen@gmail.com>
2024-04-17 14:41:12 +0300

    Lisätty värien valinta, suorakulmion ja ympyrän piirtäminen sekä lisätty tekstiä nappeihin. Pari asiaa hard-coded, jotka voisi muuttaa modulaarisemmiksi.

--- src/main/java/org/openjfx/App.java
+++ src/main/java/org/openjfx/App.java
@@ -4,11 +4,14 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
+import javafx.scene.control.ColorPicker;
 import javafx.scene.control.ComboBox;
 import javafx.scene.layout.*;
 import javafx.event.*;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.canvas.*;
+import javafx.scene.text.*;
 
 
 /**
@@ -16,13 +19,20 @@ import javafx.scene.canvas.*;
  */
 public class App extends Application {
 
+    //Universaaleja muuttujia, joita käyttää moni ohjelman metodi
     double brushSize;
+    javafx.scene.paint.Color c = javafx.scene.paint.Color.BLACK;
+    boolean isBrush = true;
+    String shapeSelection = "";
+
+    //tässä alla hiirtä seuraavia muuttujia
+    double startX;
+    double startY;
 
     @Override
     public void start(Stage stage) throws Exception {
         BorderPane root = new BorderPane();
         StackPane leftPane = new StackPane();
-        
 
         //Setup Canvas for drawing
         Canvas canvas = new Canvas(1280, 720);
@@ -33,23 +43,6 @@ public class App extends Application {
 
         root.setCenter(canvas);
 
-        Button btn1 = new Button("Bigger");
-        StackPane.setMargin(btn1, new javafx.geometry.Insets(100, 0, 50, 0));
-        Button btn2 = new Button("Smaller");
-        StackPane.setMargin(btn2, new javafx.geometry.Insets(50, 0, 100, 0));
-        
-
-        btn1.setOnAction(new EventHandler<ActionEvent>() {
-            public void handle(ActionEvent arg0) {
-                brushSize += 1;
-            }
-        });
-        btn2.setOnAction(new EventHandler<ActionEvent>() {
-            public void handle(ActionEvent arg0) {
-                brushSize -= 1;
-            }
-        });
-
         ComboBox<Double> brushDropDown = new ComboBox<>();
         brushDropDown.getItems().addAll(
             1.0,
@@ -58,33 +51,117 @@ public class App extends Application {
             4.0,
             5.0
         );
-        StackPane.setMargin(brushDropDown, new javafx.geometry.Insets(50, 0, 50, 0));
-        brushDropDown.setValue(3.0);
+        StackPane.setMargin(brushDropDown, new javafx.geometry.Insets(50, 0, 100, 0));
+        StackPane.setAlignment(brushDropDown, Pos.CENTER_LEFT);
+        brushDropDown.setValue(brushSize);
+        
 
         brushDropDown.setOnAction(event -> {
             Double selectedOption = brushDropDown.getSelectionModel().getSelectedItem();
             brushSize = selectedOption;
         });
 
+        
+        //Värin valinta
+        final ColorPicker colorPicker = new ColorPicker();
+        colorPicker.setValue(c);
+        colorPicker.setOnAction(event -> {
+            c = colorPicker.getValue();
+        });
+        //Värin nappi
+        colorPicker.getStyleClass().add("button");
+        StackPane.setMargin(colorPicker, new javafx.geometry.Insets(100, 50, 50, 0));
+        
+
 
         //EVENT HANDLERS FOR MOUSE PRESSED AND DRAGGED
         //USES ARROW FUNCTION ->
         canvas.setOnMousePressed(event -> {
-            gc.beginPath();
-            gc.moveTo(event.getX(), event.getY()); //Aloittaa pathin ja siirtää sitä mihin event-muuttuja liikkuu AKA hiiri
-            gc.setStroke(javafx.scene.paint.Color.BLACK); //Luo mustaa väriä Strokeksi - tämä pitää muuttaa seuraamaan valittua väriä!!
-            gc.setLineWidth(brushSize); //BRUSH SIZE - tämä pitää muuttaa seuraamaan valittua kokoa!!
+            if(isBrush) {
+                gc.beginPath();
+                gc.moveTo(event.getX(), event.getY()); //Aloittaa pathin ja siirtää sitä mihin event-muuttuja liikkuu AKA hiiri
+                gc.setStroke(c); //Luo väriä Strokeksi
+                gc.setLineWidth(brushSize); //BRUSH SIZE
+            } else if (!isBrush) {
+                startX = event.getX();
+                startY = event.getY();
+            }
         });
         canvas.setOnMouseDragged(event -> {
-            gc.lineTo(event.getX(), event.getY()); //Luo "viivaa" sinne, mihin hiiri liikkuu pisteestä toiseen.
-            gc.stroke();    //Luo lopullisen "piirtämisen"
+            if(isBrush) {    
+                gc.lineTo(event.getX(), event.getY()); //Luo "viivaa" sinne, mihin hiiri liikkuu pisteestä toiseen.
+                gc.stroke();    //Luo lopullisen "piirtämisen"
+            }
         });
 
+        canvas.setOnMouseReleased(event -> {
+            if(!isBrush) {
+                gc.setLineWidth(brushSize);
+                if(shapeSelection=="Rectangle with Fill") {
+                    gc.setFill(c);
+                    gc.fillRect(startX, startY, event.getX()-startX, event.getY()-startY);
+                } else if (shapeSelection=="Circle with Fill") {
+                    gc.setFill(c);
+                    gc.fillOval(startX, startY, event.getX()-startX, event.getY()-startY);
+                } else if (shapeSelection=="Rectangle Outline") {
+                    gc.setStroke(c);
+                    gc.strokeRect(startX, startY, event.getX()-startX, event.getY()-startY);
+                } else if (shapeSelection=="Circle Outline") {
+                    gc.setStroke(c);
+                    gc.strokeOval(startX, startY, event.getX()-startX, event.getY()-startY);
+                }
+            }
+        });
 
+        /*//Nappi muodon piirtoa varten
+        Button shapeDraw = new Button("Shape");
+        shapeDraw.setOnAction(event -> {
+            isBrush = false;
+        });*/
+        ComboBox<String> shapeDropDown = new ComboBox<>();
+        shapeDropDown.getItems().addAll(
+            "Rectangle with Fill",
+            "Circle with Fill",
+            "Rectangle Outline",
+            "Circle Outline"
+        );
+        shapeDropDown.setValue("Rectangle with Fill");
+        shapeDropDown.setOnAction(event -> {
+            String selectedString = shapeDropDown.getSelectionModel().getSelectedItem();
+            shapeSelection = selectedString;
+            isBrush = false;
+        });
 
-        leftPane.getChildren().add(btn1);
-        leftPane.getChildren().add(btn2);
-        leftPane.getChildren().addAll(brushDropDown);
+        //Nappi vapaapiirtoa varten
+        Button freeDraw = new Button("Brush");
+        freeDraw.setOnAction(event -> {
+            isBrush = true;
+        });
+
+        //Teksti Brush Sizen valinnalle
+        Text brushSizeText = new Text("Brush Size");
+        StackPane.setMargin(brushSizeText, new javafx.geometry.Insets(0, 0, 90, 0));
+        StackPane.setAlignment(brushSizeText, Pos.CENTER_LEFT);
+        leftPane.getChildren().add(brushSizeText);
+
+        //Teksti Brush Color valinnalle
+        Text brushColorText = new Text("Brush Color");
+        StackPane.setMargin(brushColorText, new javafx.geometry.Insets(0, 0, -10, 0));
+        StackPane.setAlignment(brushColorText, Pos.CENTER_LEFT);
+        leftPane.getChildren().add(brushColorText);
+
+        leftPane.getChildren().add(brushDropDown);
+        brushDropDown.setMinWidth(110);
+
+        leftPane.getChildren().add(colorPicker);
+
+        StackPane.setMargin(shapeDropDown, new javafx.geometry.Insets(0, 0, -150, 0));
+        StackPane.setAlignment(shapeDropDown, Pos.CENTER_LEFT);
+
+        StackPane.setMargin(freeDraw, new javafx.geometry.Insets(0, 0, -200, 0));
+        StackPane.setAlignment(freeDraw, Pos.CENTER_LEFT);
+        leftPane.getChildren().addAll(shapeDropDown, freeDraw);
+
         root.setLeft(leftPane);
 
         
