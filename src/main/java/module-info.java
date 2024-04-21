0a080bfc5c20e86ef459dc568472b1a7b7535122
Jaakko Mikkola <jaakkomikkola@hotmail.com>
2024-04-21 16:44:55 +0300

    Kuvan lisäys

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,9 @@
 module org.openjfx {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
+    
+    
 
     opens org.openjfx to javafx.fxml;
     exports org.openjfx;
